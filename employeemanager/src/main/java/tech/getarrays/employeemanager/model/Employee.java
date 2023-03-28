a1b526d3756d14c066dc878b57f52d42d512e022
kweisVee <korekweis@gmail.com>
2023-03-28 14:06:15 -0700

    initialization

---
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,11 @@
+package tech.getarrays.employeemanager.model;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+}
